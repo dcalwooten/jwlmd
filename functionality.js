9b8f97cc2965b3b2044590c84d7ef053f49a2031
Keith <ktugg613@gmail.com>
2021-03-16 16:45:59 -0400

    Add band member bios and switch ability.
    Added a new section to biography section for individual member photos and bios. Users can click on a member's photo to display the member's bio info below.

--- functionality.js
+++ functionality.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function()
 
     window.onscroll = function() { stickyNav() };
 
+    // trigger scroll on load in case user loads from an anchor point lower on page
     window.dispatchEvent(new CustomEvent('scroll'))
 
     function stickyNav() {
@@ -30,6 +31,28 @@ window.addEventListener('load', function()
         {
             menu.classList.add('is-active');
             this.setAttribute('aria-expanded', 'true');
+        }
+    });
+
+    // functionality for switching individual member bios
+    document.getElementById('member-images').addEventListener('click', function(e)
+    {
+        var event = e || window.event,
+            target = event.target;
+
+        if ('bioId' in target.dataset)
+        {
+            let bioId = target.dataset.bioId,
+                bioIdText = document.getElementById(bioId),
+                bioTextContainer = document.getElementById('member-bio-text'),
+                activeBioText = bioTextContainer.getElementsByClassName('active')[0];
+
+            // if text related to clicked image isn't active, make active
+            if (activeBioText.getAttribute('id') !== bioId)
+            {
+                activeBioText.classList.remove('active');
+                bioIdText.classList.add('active');
+            }
         }
     });
 });

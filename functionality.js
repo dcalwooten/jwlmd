f628ab4c41426127ae856d923906eaef756b522e
Keith <ktugg613@gmail.com>
2022-12-29 15:38:18 -0500

    Refactor member bio selection logic with petite-vue component.
    Javascript autoformatting.

--- functionality.js
+++ functionality.js
@@ -1,58 +1,30 @@
-window.addEventListener('load', function()
-{
+window.addEventListener('load', function () {
     var navBar = document.getElementById('top-nav'),
         navOffset = navBar.offsetTop;
 
-    window.onscroll = function() { stickyNav() };
+    window.onscroll = function () { stickyNav() };
 
     // trigger scroll on load in case user loads from an anchor point lower on page
     window.dispatchEvent(new CustomEvent('scroll'))
 
     function stickyNav() {
-        if (window.pageYOffset >= navOffset)
-        { navBar.classList.add('sticky'); }
+        if (window.pageYOffset >= navOffset) { navBar.classList.add('sticky'); }
 
-        else
-        { navBar.classList.remove('sticky'); }
+        else { navBar.classList.remove('sticky'); }
     }
 
     var menu = document.getElementById('menu'),
         menuToggle = document.getElementById('menu-toggle')
 
-    menuToggle.addEventListener('click', function()
-    {
-        if (menu.classList.contains('is-active'))
-        {
+    menuToggle.addEventListener('click', function () {
+        if (menu.classList.contains('is-active')) {
             menu.classList.remove('is-active');
             this.setAttribute('aria-expanded', 'false');
         }
 
-        else
-        {
+        else {
             menu.classList.add('is-active');
             this.setAttribute('aria-expanded', 'true');
-        }
-    });
-
-    // functionality for switching individual member bios
-    document.getElementById('member-images').addEventListener('click', function(e)
-    {
-        var event = e || window.event,
-            target = event.target;
-
-        if ('bioId' in target.dataset)
-        {
-            let bioId = target.dataset.bioId,
-                bioIdText = document.getElementById(bioId),
-                bioTextContainer = document.getElementById('member-bio-text'),
-                activeBioText = bioTextContainer.getElementsByClassName('active')[0];
-
-            // if text related to clicked image isn't active, make active
-            if (activeBioText.getAttribute('id') !== bioId)
-            {
-                activeBioText.classList.remove('active');
-                bioIdText.classList.add('active');
-            }
         }
     });
 });

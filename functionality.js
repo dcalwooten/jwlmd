9336edb8f421b7ac86371ea1a9d13b827bd70266
Keith <ktugg613@gmail.com>
2023-09-06 09:21:34 -0400

    Restyling (#7)
    * Add new font and styling for headers.
    
    Set background to white.
    
    Remove max-width and margin for section images.
    
    Replace group band photo with new one, along with new alt text.
    
    * Remove Josh and image from bio.
    
    Add placeholders for Tom in bio and photo.
    
    * New top banner and textured band photo.
    
    * Restructure and restyle show date section.
    
    Add hamburger icon for mobile menu.
    
    * Restructure show dates section.
    
    Add hamburger menu icon for mobile nav.
    
    * Remove unused font
    
    * New mobile nav icon.
    
    Restyling toggle button.
    
    Restyling mobile headers.
    
    * Remove unused image
    
    * Add functionality to close mobile nav if clicking outside of it.

--- functionality.js
+++ functionality.js
@@ -16,7 +16,8 @@ window.addEventListener('load', function () {
     var menu = document.getElementById('menu'),
         menuToggle = document.getElementById('menu-toggle')
 
-    menuToggle.addEventListener('click', function () {
+    menuToggle.addEventListener('click', function (e) {
+        e.stopPropagation();
         if (menu.classList.contains('is-active')) {
             menu.classList.remove('is-active');
             this.setAttribute('aria-expanded', 'false');
@@ -25,6 +26,15 @@ window.addEventListener('load', function () {
         else {
             menu.classList.add('is-active');
             this.setAttribute('aria-expanded', 'true');
+        }
+    });
+
+    // collapse mobile menu when clicking outside of it
+    document.addEventListener('click', function (e) {
+        if (menu.classList.contains('is-active') &&
+            e.target.closest('#menu') == null) {
+            menu.classList.remove('is-active');
+            menuToggle.setAttribute('aria-expanded', 'false');
         }
     });
 });
